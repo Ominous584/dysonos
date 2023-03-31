e367ae514291774c7a7d67182de75cd96d129efa
Aditya Giri <adityagiri01101001@gmail.com>
2023-03-31 14:34:30 +0530

    added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,123 +1,391 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+//import styles from '@/styles/globals.css'
 import styles from '@/styles/Home.module.css'
-
+import Script from 'next/script'
+import { useEffect, useState } from 'react'
+//import banner1 from '@/pages/images/banner.png'
+import Link from 'next/link';
+import { RxHamburgerMenu, RxWidth } from 'react-icons/rx';
+import { VscAccount } from "react-icons/vsc";
+import { CiLogout, CiSettings, CiUser } from "react-icons/ci";
+import { BiTrendingUp } from "react-icons/bi";
+import { SlLike } from "react-icons/sl";
 const inter = Inter({ subsets: ['latin'] })
 
+
+
+// Import the functions you need from the SDKs you need
+import  {initializeApp}  from 'firebase/app';
+import { getDatabase, ref, onValue, get, child, orderByChild, query, limitToLast } from "firebase/database";
+//import { getStorage, uploadBytes, ref as Sref, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
+import { getStorage, uploadBytes, ref as Sref, getDownloadURL } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+  apiKey: "AIzaSyBOhWfbhSdOhUvJAw2qF3qZaCGpKdr6Rgg",
+  authDomain: "blog-5ec50.firebaseapp.com",
+  projectId: "blog-5ec50",
+  storageBucket: "blog-5ec50.appspot.com",
+  messagingSenderId: "973166205545",
+  appId: "1:973166205545:web:8ff6a22e3b97bf7cf3e0f8",
+  measurementId: "G-HVFHTVQKYS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+const db = getDatabase(app);
+const rf = ref(db, "Articles/posted");
+const storage = getStorage();
+
+
+
+const list = [];
+const mostViewedPosts = query(ref(db, "posted"), orderByChild('timestamp'));
+const mostViewedPosts1 = query(ref(db, "posted"), orderByChild('views/views', 'desc'));
+console.log(mostViewedPosts)
+console.log(mostViewedPosts1)
+get(mostViewedPosts).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+    console.log("title is ", snapshot.child("body").val())
+    snapshot.forEach((childSnapshot) => {
+      console.log("one title", childSnapshot.key)
+      console.log("one body", childSnapshot.child("body").val())
+      const childKey = childSnapshot.key;
+              const childData = childSnapshot.val();
+
+              console.log(childKey)
+              list.push(childKey)
+              var container = document.createElement("div")
+              var boco = document.createElement("div")
+              var listi = document.createElement("li")
+              container.classList.add("container")
+              listi.classList.add("listi")
+
+
+          
+          
+              
+              var a = document.createElement("a");
+              var checki = sessionStorage.getItem("checker112")
+              
+              a.textContent = childKey;
+              //a.classList.add("entry")
+              //a.classList.add("entry2")
+              a.classList.add("entry1")
+              a.style.listStyle = "none"
+              
+              a.setAttribute('href', "posts/" + childKey);
+              
+
+              var checkii = sessionStorage.getItem("check123")
+
+              
+
+              function resclick() {
+                sessionStorage.setItem("article", childKey);
+                window.open("viewer.html", '_self');
+              }
+
+              var har = document.createElement("hr")
+              har.style.marginBottom = "2vw"
+              har.style.marginTop = "1vw"
+              har.style.marginLeft = "-10vw"
+              har.classList.add("har")
+              
+              try{
+              var bod = childSnapshot.child("body").val()
+              var bode = bod.slice(0, 30)
+              var boded = bode + "...."
+              var entry1 = document.createElement('li');
+              entry1.appendChild(document.createTextNode(boded));
+              entry1.addEventListener('click', resclick);
+              entry1.style.listStyle = "none"
+              var banner = document.createElement("img")
+              getDownloadURL(Sref(storage, "posted/" + childKey + '/banner.png')).then(function(url) {
+                
+                var test = url;
+                console.log(test)
+                banner.src = test;
+                banner.classList.add("lbanner")
+
+              }).catch(function(error) {
+                console.log("error!")
+              });
+
+              
+              
+
+              //entry.style.boxShadow = "0 0 15px 4px rgba(0,0,0,0.06)"
+              
+              //entry.style.marginLeft = "-20vw"
+              entry1.classList.add("aaa");
+              a.style.listStyle = "none" 
+              boco.classList.add("boco")
+              boco.append(a)
+              
+              boco.appendChild(entry1)
+              //container.appendChild(har)
+              container.appendChild(boco)
+              container.appendChild(banner)
+              var liner = document.createElement("hr")
+              listi.appendChild(container)
+              
+              listi.style.listStyle = "none"
+              res.appendChild(listi)}catch(err){
+                console.log('error')                
+              }
+    })
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+get(mostViewedPosts1).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+    console.log("title is ", snapshot.child("body").val())
+    snapshot.forEach((childSnapshot) => {
+      console.log("one title", childSnapshot.key)
+      console.log("one body", childSnapshot.child("body").val())
+      const childKey = childSnapshot.key;
+              const childData = childSnapshot.val();
+
+              console.log(childKey)
+              list.push(childKey)
+              var container = document.createElement("div")
+              var boco = document.createElement("div")
+              var listi = document.createElement("li")
+              container.classList.add("trending_container")
+              listi.classList.add("listi")
+
+
+          
+          
+              
+              var a = document.createElement("a");
+              var checki = sessionStorage.getItem("checker112")
+              
+              a.textContent = childKey;
+              //a.classList.add("entry")
+              //a.classList.add("entry2")
+              a.classList.add("trend_title")
+              a.style.listStyle = "none"
+              
+              a.setAttribute('href', "posts/" + childKey);
+              
+
+              var checkii = sessionStorage.getItem("check123")
+
+              
+
+              function resclick() {
+                sessionStorage.setItem("article", childKey);
+                window.open("viewer.html", '_self');
+              }
+
+              var har = document.createElement("hr")
+              har.style.marginBottom = "2vw"
+              har.style.marginTop = "1vw"
+              har.style.marginLeft = "-10vw"
+              har.classList.add("har")
+              var rel = document.getElementById("rel")
+              
+              try{
+              var bod = childSnapshot.child("body").val()
+              var bode = bod.slice(0, 30)
+              var boded = bode + "...."
+              var entry1 = document.createElement('li');
+              entry1.appendChild(document.createTextNode(boded));
+              entry1.addEventListener('click', resclick);
+              entry1.style.listStyle = "none"
+              var banner = document.createElement("img")
+              
+
+              
+              
+
+              //entry.style.boxShadow = "0 0 15px 4px rgba(0,0,0,0.06)"
+              
+              //entry.style.marginLeft = "-20vw"
+              entry1.classList.add("trending_body");
+              a.style.listStyle = "none" 
+              
+              //container.appendChild(har)
+              container.appendChild(a)
+              container.appendChild(entry1)
+              
+              var liner = document.createElement("hr")
+              listi.appendChild(container)
+              
+              listi.style.listStyle = "none"
+              rel.appendChild(listi)}catch(err){
+                console.log('error')                
+              }
+    })
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+if (typeof window !== "undefined") {
+  window.addEventListener('DOMContentLoaded', (event) => {
+    
+    var res = document.getElementById("res")
+    document.body.style.display = 'block'
+    onValue(rf, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+              
+        // ...
+      });
+    }, {
+      onlyOnce: true
+    });
+  });
+    
+
+  
+}
+
+
+//Acount shower and stuff -----------------------------------------------------------------
+//----------------------------------------------------------Account-----------------------
+if (typeof window !== 'undefined') {
+  window.addEventListener('DOMContentLoaded', (event) => {
+    
+  var user = localStorage.getItem("user")
+  
+
+  if (user == null || user == undefined || user == "") {
+    document.getElementById("user").style.display = "none"
+    console.log("not logged in")
+    document.getElementById('starter').addEventListener('click', function() {
+      window.open("signup", '_self')
+    })
+    
+  } else {
+    console.log("logged in + " + user)
+    document.getElementById('logol').style.display = 'none'
+    document.getElementById('logol1').style.display = 'none'
+    var imma = "https://ui-avatars.com/api/?name=" + user
+    console.log(imma)
+    document.getElementById("imm").src = imma
+    document.getElementById("imm2").src = imma
+    document.getElementById('starter').addEventListener('click', function() {
+      window.open("signup", '_self')
+    })
+    
+    
+    
+    
+    console.log(imma)
+  }
+  })}
+
+
 export default function Home() {
+
+  const [showMe, setShowMe] = useState(false);
+  function toggle(){
+    setShowMe(!showMe);
+    document.getElementById('nama').innerHTML = user
+    document.getElementById('imm').style.display = 'none'
+  }
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+ 
+    <main >
+          <div id='progressbar'></div>
+          <div id='scrollPath'></div>
+    
+
+
+
+        
+          <div className='navbar'>
+            <div className='logo'>Dysonos</div>
+            <div className='nav-links'>
+           
+              <Link href='articles' className='del safe'>Crowd articles</Link>
+              <Link href='#' className='del safe'>About</Link>
+              <Link href='#' className='del safe'>Contact</Link>
+              <Link href='login' className='del alt' id='logol'>Login</Link>
+              <Link href='signup' className='del alt' id='logol1'>Sign up</Link>
+              
+            </div>
+            <div id='user' className='user'>
+              <img className='shower' id='imm'></img>
+              <div className='drop' >
+                <h1 id='abaca'></h1>
+                <hr></hr>
+                <Link href='profile' className='droper'><CiUser  /> My profile</Link>
+                <hr></hr>
+                <Link href='settings' className='droper'><CiSettings /> Settings</Link>
+                <hr></hr>
+                <Link href='logout' className='droper'><CiLogout /> Logout</Link>
+                
+                </div>         
+              </div>
+            <div className='hamburger'>
+              <button className='ham' onClick={toggle} style={{ RxWidth: '10vw', fontSize: '9vw', marginLeft: '55vw', marginBottom: '4vw', border: 'none', width: '13vw'}} ><RxHamburgerMenu  /></button>
+            </div>
+            <div className='menu' style={{ display: showMe ? "block" : "none" }}>
+              <div className='profo'>
+                <img className='shower2' id='imm2'></img>
+                <div className='nama'><h1 id='nama' ></h1></div>
+              </div>
+              
+              <Link href='articles' className='del1 safe'>Articles</Link>
+              <Link href='#' className='del1  safe'>About</Link>
+              <Link href='#' className='del1 safe'>Contact</Link>
+              <Link href='login' className='del1 alt'>Login</Link>
+              <Link href='signup' className='del1 alt'>Sign up</Link>
+              
+            </div>
           </div>
+          
+
+        <div className='deez'>
+          <h1 className='techz'>Great Tech Blogs</h1>
+          <button className='start' id='starter'>Start reading</button>
+        </div>
+        <hr className='seperator'></hr>
+        <div className='trend'>
+          <h1><BiTrendingUp /> Trending</h1>
+          <ul id='rel'></ul>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <h1 className='latest'>Latest</h1>
+        <div className='aspects'>
+          <h1 className='discohead'>Discover posts of your interest.</h1>
+          <Link href='topics/technology' className='discotop'>Technology</Link>
+          <Link href='topics/programming' className='discotop'>Programming</Link>
+          <Link href='topics/machinelearning' className='discotop'>Machine learning</Link>
+          <Link href='topics/health' className='discotop'>Health</Link>
+          <Link href='topics/sports' className='discotop'>Sports</Link>
+          <Link href='topics/politics' className='discotop'>Politics</Link>
+          <Link href='topics/events' className='discotop'>Events</Link>
+          <hr className='discomar'></hr>
+
         </div>
+        
+        <ul id='res'></ul>
+        
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+  </main>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
+    
+      
+    
   )
 }
