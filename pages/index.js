89891ce28fa4b308ee97e0ccdab804b724ebf491
Aditya Giri <adityagiri01101001@gmail.com>
2023-04-04 16:35:34 +0530

    errer

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 //import styles from '@/styles/globals.css'
-import styles from '@/styles/Home.module.css'
+
 import Script from 'next/script'
 import { useEffect, useState } from 'react'
 //import banner1 from '@/pages/images/banner.png'
